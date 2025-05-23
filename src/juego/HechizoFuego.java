aaf0e4cdbf38a90144404880e9a83cefb00d4b53
Paperbox12 <lucasbox1665@gmail.com>
2025-05-23 19:16:19 -0300

    ajuste de colisión con las piedras

--- src/juego/HechizoFuego.java
+++ src/juego/HechizoFuego.java
@@ -1,6 +1,7 @@
 package juego;
 import java.awt.Image;
 
+import entorno.Entorno;
 import entorno.Herramientas;
 
 public class HechizoFuego {
@@ -16,7 +17,19 @@ public class HechizoFuego {
 		
 		this.x=x;
 		this.y=y;
-		this.HechizoFuego=Herramientas.cargarImagen(null);
+		this.HechizoFuego=Herramientas.cargarImagen("HechizoBasico.gif");
 		this.escala=0.4;
+	}
+	
+	public void dibujarse(Entorno entorno) {
+		entorno.dibujarImagen(HechizoFuego, x, y, escala);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
 	}
 }
