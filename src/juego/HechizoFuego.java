54f0d9d1a16a901b4cce857e495c98e70da1284a
Paperbox12 <lucasbox1665@gmail.com>
2025-05-23 19:02:26 -0300

    creacion de hechizo basico

---
+++ src/juego/HechizoFuego.java
@@ -0,0 +1,22 @@
+package juego;
+import java.awt.Image;
+
+import entorno.Herramientas;
+
+public class HechizoFuego {
+
+
+	private int x;
+	private int y;
+	private Image HechizoFuego;
+	private double escala;
+	
+	
+	public HechizoFuego(){
+		
+		this.x=x;
+		this.y=y;
+		this.HechizoFuego=Herramientas.cargarImagen(null);
+		this.escala=0.4;
+	}
+}
