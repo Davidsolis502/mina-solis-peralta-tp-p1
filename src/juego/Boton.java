3b60d53734637905e0e159e7e163ab50ce236b88
david <solisbsas@gmail.com>
2025-05-24 10:54:06 -0300

    cambios clase boton falta interacciones

--- src/juego/Boton.java
+++ src/juego/Boton.java
@@ -10,18 +10,39 @@ public class Boton {
 	private int x;
 	private int y;
 	private Image boton;
+	private Image boton1;
 	private double escala;
+	private boolean sepresiono;
 
 	public Boton(int x,int y){
 		this.x=x;
 		this.y=y;
 		this.escala= 0.1;
 		this.boton=Herramientas.cargarImagen("Hechizo-desactivado.png");
+		this.boton1=Herramientas.cargarImagen("Hechizo-activado.png");
+		this.sepresiono=false;
 		
 	}
 	public void dibujarse(Entorno entorno) {
-		entorno.dibujarImagen(boton, x, y,0,escala);
+		if (sepresiono) {
+			entorno.dibujarImagen(boton1, x, y, 0, escala); // Imagen cuando se presiona
+		} else {
+			entorno.dibujarImagen(boton, x, y, 0, escala);  // Imagen por defecto
+		}
 	}
+	public void presionar() {
+		sepresiono = true;
+	}
+	
+	public void soltar() {
+		sepresiono = false;
+	}
+	
+	public boolean estaPresionado() {
+		return sepresiono;
+	}
+	
+	
 
 	public int getX() {
 		return x;
